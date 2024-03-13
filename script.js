142a8d632762542204dffda41b8bf5b0a6b2e81c
amirhosein06 <amirhoseinzolfaghari06@gmail.com>
2024-03-13 13:19:10 +0330

    add counter code

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-
-//add persian number
-var replaceDigits = function() {
-    var map = ["&\#1776;","&\#1777;","&\#1778;","&\#1779;","&\#1780;","&\#1781;","&\#1782;","&\#1783;","&\#1784;","&\#1785;"]
-    document.body.innerHTML = document.body.innerHTML.replace(/\d(?=[^<>]*(<|$))/g, function($0) { return map[$0]});
-}
-window.onload = replaceDigits;
-
 //words data
 const singDots = ["ب", "ج", "خ", "ز", "ذ", "ض", "ظ", "ن", "ف", "غ"],
 trieDots = ["پ", "چ", "ژ", "ش", "ث"],
@@ -13,6 +5,40 @@ doublDots = ["ت", "ق"],
 noDot = ["ی"];
 
 //document element
-const dotinput = document.querySelector(".counter_box input"),
-preview = document.querySelector(".preview");
+const dotinput = document.querySelector(".counter_box input");
+const preview = document.querySelector(".preview");
 
+//counter
+dotinput.addEventListener("input",(e)=>{
+   const inputval = e.target.value,
+   valuesplit = inputval.split("");
+   var count = 0;
+   
+   valuesplit.forEach((item,index) => {
+     if (singDots.includes(item)) {
+        count += 1;
+     };
+     if (doublDots.includes(item)) {
+        count += 2;
+     };
+     if (trieDots.includes(item)) {
+        count += 3;
+     };
+     if (noDot.includes(item) && valuesplit.length > 1 && index !== (valuesplit.length -1)) {
+        count += 2;
+     };
+   });
+
+   preview.innerHTML = count;
+});
+
+
+
+
+
+//add persian number
+// function replaceDigits() {
+//     var map = ["&\#1776;","&\#1777;","&\#1778;","&\#1779;","&\#1780;","&\#1781;","&\#1782;","&\#1783;","&\#1784;","&\#1785;"]
+//     document.body.innerHTML = document.body.innerHTML.replace(/\d(?=[^<>]*(<|$))/g, function($0) { return map[$0]});
+// };
+// window.onload = replaceDigits;
